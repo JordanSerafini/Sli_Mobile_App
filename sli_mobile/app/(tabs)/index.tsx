23f4f8ad1b3677a083cf9f2e7618e15f145031d4
Jordan S <jordanserafini.74@gmail.com>
2024-07-07 09:48:43 +0200

    :sparkles: netivewind :sparkles:

--- sli_mobile/app/(tabs)/index.tsx
+++ sli_mobile/app/(tabs)/index.tsx
@@ -2,8 +2,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import tw from 'twrnc';
+import { url } from '../utils/url';
 
 export default function App() {
+
+  const dataClient = async () => {
+    const response = await fetch(url.client + '/customers');
+    const data = await response.json();
+    console.log(data);
+  }
+
+  dataClient();
+
   return (
     <View style={tw`flex-1 items-center justify-center bg-red-500`}>
       <Text style={tw`text-white text-lg`}>Hello, Tailwind CSS with Expo!</Text>
---
+++ sli_mobile/app/utils/url.ts
@@ -0,0 +1,6 @@
+export const url = {
+    auth: 'http://localhost:5002',
+    user: 'http://localhost:5001',
+    client: 'http://localhost:5003',
+    item: 'http://localhost:5004',
+}
