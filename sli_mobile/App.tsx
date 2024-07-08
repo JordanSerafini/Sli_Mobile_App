9fb8a5c96da54b8dcbaba2231db761ea9e18d0dc
JordanSerafini <jordan@solution-logique.fr>
2024-07-08 13:44:02 +0200

    setup ok + nativewind for mobile + web :sparkles: :sparkles:

---
+++ sli_mobile/App.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import { Text, View } from "react-native";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text>Open up App.js to start working on your app!!</Text>
+    </View>
+  );
+}
