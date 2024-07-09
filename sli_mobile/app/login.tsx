b256d3ce02c6d414dd06f1280d001591c6d32181
JordanSerafini <jordan@solution-logique.fr>
2024-07-09 09:42:36 +0200

    push

--- sli_mobile/app/login.tsx
+++ sli_mobile/app/login.tsx
@@ -1,40 +1,44 @@
-import React, { useState } from 'react';
-import { View, Text, Button, TextInput, Image, StyleSheet } from 'react-native';
-import { useRouter } from 'expo-router';
+import React, { useState } from "react";
+import { View, Text, Button, TextInput, Image, StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
 
-const sli = require('./assets/sli.jpg');
+const sli = require("./assets/sli.jpg");
 
 const LoginScreen: React.FC = () => {
-  const [email, setEmail] = useState('jordan@solution-logique.fr');
-  const [password, setPassword] = useState('pass123');
+  const [email, setEmail] = useState("jordan@solution-logique.fr");
+  const [password, setPassword] = useState("pass123");
 
   const router = useRouter();
 
   const handleLogin = () => {
-    router.push('/home');
+    router.push("/home");
   };
 
   return (
-    <View >
-      <Image source={sli} className='' />
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-      />
-      <TextInput
-        value={password}
-        onChangeText={setPassword}
-        placeholder="Password"
-        secureTextEntry
-      />
-      <Button title="Login" onPress={handleLogin} />
+    <View className="h-full w-full flex items-center justify-center">
+      <View>
+        <Image source={sli} className="w-9/10" />
+      </View>
+      <View>
+        <View>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <TextInput
+            value={password}
+            onChangeText={setPassword}
+            placeholder="Password"
+            secureTextEntry
+          />
+        </View>
+        <Button title="Login" onPress={handleLogin} />
+      </View>
     </View>
   );
 };
-
-
 
 export default LoginScreen;
