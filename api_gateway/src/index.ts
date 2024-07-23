c66bd5834b441ece9b9c82b370d2738d892b6f0a
JordanSerafini <jordan@solution-logique.fr>
2024-07-23 17:27:57 +0200

    api gateway :construction:

--- api_gateway/src/index.ts
+++ api_gateway/src/index.ts
@@ -15,41 +15,45 @@ const services = {
   itemService: process.env.ITEM_SERVICE_URL,
 };
 
+// Vérifiez les URLs des services
+Object.keys(services).forEach(service => {
+  if (!services[service]) {
+    console.error(`Missing URL for ${service}`);
+  }
+});
+
+console.log('User Service URL:', services.userService);
+console.log('Auth Service URL:', services.authService);
+console.log('Client Service URL:', services.clientService);
+console.log('Item Service URL:', services.itemService);
+
 // Proxy pour le service des utilisateurs
 app.use('/users', createProxyMiddleware({
   target: services.userService,
   changeOrigin: true,
-  pathRewrite: {
-    '^/users': '', 
-  },
+  pathRewrite: {'^/users': ''},
 }));
 
 // Proxy pour le service d'authentification
 app.use('/auth', createProxyMiddleware({
   target: services.authService,
   changeOrigin: true,
-  pathRewrite: {
-    '^/auth': '', 
-  },
+  pathRewrite: {'^/auth': ''},
 }));
 
 // Proxy pour le service des clients
 app.use('/clients', createProxyMiddleware({
   target: services.clientService,
   changeOrigin: true,
-  pathRewrite: {
-    '^/clients': '', 
-  },
-  logLevel: 'debug',
+  pathRewrite: {'^/clients': ''},
+  logLevel: 'debug', // Ajoutez le niveau de log
 }));
 
 // Proxy pour le service des items
 app.use('/items', createProxyMiddleware({
   target: services.itemService,
   changeOrigin: true,
-  pathRewrite: {
-    '^/items': '', 
-  },
+  pathRewrite: {'^/items': ''},
 }));
 
 app.listen(PORT, () => {
