be4ddddb2fb4ac59d69fd91ab771234cd0f19f0a
JordanSerafini <jordan@solution-logique.fr>
2024-07-25 16:17:59 +0200

    :bug: fix write csv when customer add

--- backend/client_api/src/model/client_model.ts
+++ backend/client_api/src/model/client_model.ts
@@ -3,33 +3,32 @@ import { pgClient } from "../client/client";
 import redisClient from "../client/redisClient";
 import * as fs from "fs";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 // Get the directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const client_model = {
-  
   async getAllCustomers(req: Request, res: Response) {
     const query = `SELECT * FROM "Customer" ORDER BY "Name" ASC;`;
-    const cacheKey = 'all_customers';
+    const cacheKey = "all_customers";
 
     try {
       const cachedData = await redisClient.get(cacheKey);
 
       if (cachedData) {
-        console.log('Cache hit');
-        return res.json(JSON.parse(cachedData)); 
+        console.log("Cache hit");
+        return res.json(JSON.parse(cachedData));
       } else {
-        console.log('Cache miss');
+        console.log("Cache miss");
 
         const result = await pgClient.query(query);
-        await redisClient.setEx(cacheKey, 3600, JSON.stringify(result.rows)); 
+        await redisClient.setEx(cacheKey, 3600, JSON.stringify(result.rows));
         res.json(result.rows);
       }
     } catch (err) {
-      console.error('Error fetching customers:', err);
+      console.error("Error fetching customers:", err);
       res.status(500).json({ message: "Internal server error" });
     }
   },
@@ -43,10 +42,10 @@ const client_model = {
       const cachedData = await redisClient.get(cacheKey);
 
       if (cachedData) {
-        console.log('Cache hit');
+        console.log("Cache hit");
         return res.json(JSON.parse(cachedData));
       } else {
-        console.log('Cache miss');
+        console.log("Cache miss");
 
         const result = await pgClient.query(query, [id]);
         if (result.rows.length === 0) {
@@ -56,7 +55,7 @@ const client_model = {
         res.json(result.rows[0]);
       }
     } catch (err) {
-      console.error('Error fetching customer by ID:', err);
+      console.error("Error fetching customer by ID:", err);
       res.status(500).json({ message: "Internal server error" });
     }
   },
@@ -70,20 +69,20 @@ const client_model = {
       const cachedData = await redisClient.get(cacheKey);
 
       if (cachedData) {
-        console.log('Cache hit');
-        return res.json(JSON.parse(cachedData)); 
+        console.log("Cache hit");
+        return res.json(JSON.parse(cachedData));
       } else {
-        console.log('Cache miss');
+        console.log("Cache miss");
 
         const result = await pgClient.query(query, [name]);
         if (result.rows.length === 0) {
           return res.status(404).json({ message: "Customer not found" });
         }
-        await redisClient.setEx(cacheKey, 3600, JSON.stringify(result.rows[0])); 
+        await redisClient.setEx(cacheKey, 3600, JSON.stringify(result.rows[0]));
         res.json(result.rows[0]);
       }
     } catch (err) {
-      console.error('Error fetching customer by name:', err);
+      console.error("Error fetching customer by name:", err);
       res.status(500).json({ message: "Internal server error" });
     }
   },
@@ -97,10 +96,10 @@ const client_model = {
 
       const cachedData = await redisClient.get(cacheKey);
       if (cachedData) {
-        console.log('Cache hit');
+        console.log("Cache hit");
         return res.json(JSON.parse(cachedData));
       } else {
-        console.log('Cache miss');
+        console.log("Cache miss");
 
         let query = `SELECT * FROM "Customer"`;
         let countQuery = `SELECT COUNT(*) FROM "Customer"`;
@@ -117,7 +116,9 @@ const client_model = {
         queryParams.push(limit);
         queryParams.push(offset);
 
-        query += ` ORDER BY "Name" ASC LIMIT $${queryParams.length - 1} OFFSET $${queryParams.length}`;
+        query += ` ORDER BY "Name" ASC LIMIT $${
+          queryParams.length - 1
+        } OFFSET $${queryParams.length}`;
         countQuery += `;`;
 
         const [customerResult, totalResult] = await Promise.all([
@@ -150,93 +151,102 @@ const client_model = {
     const id = parseInt(req.params.id, 10);
     const query = `DELETE FROM "Customer" WHERE "Id" = $1 RETURNING *;`;
     try {
-        const result = await pgClient.query(query, [id]);
-        if (result.rows.length === 0) {
-            return res.status(404).json({ message: "Customer not found" });
-        }
-
-        // Invalidate the cache
-        await redisClient.del('all_customers');
-        await redisClient.del(`customer_${id}`);
-        await redisClient.del(`customers_paginated_*`);
-
-        res.json(result.rows[0]);
-    } catch (err) {
-        console.error('Error deleting customer by ID:', err);
-        res.status(500).json({ message: "Internal server error" });
-    }
-},
-
-
-async updateCustomerById(req: Request, res: Response) {
-  const id = parseInt(req.params.id, 10);
-  const { name, email } = req.body;
-  const query = `UPDATE "Customer" SET "Name" = $1, "Email" = $2 WHERE "Id" = $3 RETURNING *;`;
-  try {
-      const result = await pgClient.query(query, [name, email, id]);
+      const result = await pgClient.query(query, [id]);
       if (result.rows.length === 0) {
-          return res.status(404).json({ message: "Customer not found" });
+        return res.status(404).json({ message: "Customer not found" });
       }
 
       // Invalidate the cache
-      await redisClient.del('all_customers');
+      await redisClient.del("all_customers");
       await redisClient.del(`customer_${id}`);
       await redisClient.del(`customers_paginated_*`);
 
       res.json(result.rows[0]);
-  } catch (err) {
-      console.error('Error updating customer by ID:', err);
+    } catch (err) {
+      console.error("Error deleting customer by ID:", err);
       res.status(500).json({ message: "Internal server error" });
-  }
-},
+    }
+  },
+
+  async updateCustomerById(req: Request, res: Response) {
+    const id = parseInt(req.params.id, 10);
+    const { name, email } = req.body;
+    const query = `UPDATE "Customer" SET "Name" = $1, "Email" = $2 WHERE "Id" = $3 RETURNING *;`;
+    try {
+      const result = await pgClient.query(query, [name, email, id]);
+      if (result.rows.length === 0) {
+        return res.status(404).json({ message: "Customer not found" });
+      }
+
+      // Invalidate the cache
+      await redisClient.del("all_customers");
+      await redisClient.del(`customer_${id}`);
+      await redisClient.del(`customers_paginated_*`);
+
+      res.json(result.rows[0]);
+    } catch (err) {
+      console.error("Error updating customer by ID:", err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  },
 
   async createCustomer(req: Request, res: Response) {
     const data = req.body;
 
     try {
-        const columns = Object.keys(data)
-            .map((key) => `"${key}"`)
-            .join(", ");
-        const values = Object.values(data)
-            .map((value, index) => `$${index + 1}`)
-            .join(", ");
+      const columns = Object.keys(data)
+        .map((key) => `"${key}"`)
+        .join(", ");
+      const values = Object.values(data)
+        .map((value, index) => `$${index + 1}`)
+        .join(", ");
 
-        const query = `INSERT INTO "Customer" (${columns}) VALUES (${values}) RETURNING *`;
-        const result = await pgClient.query(query, Object.values(data));
-        
-        const newCustomer = result.rows[0];
+      const query = `INSERT INTO "Customer" (${columns}) VALUES (${values}) RETURNING *`;
+      const result = await pgClient.query(query, Object.values(data));
 
-        // Invalidate the cache
-        await redisClient.del('all_customers');
-        await redisClient.del(`customers_paginated_*`);
+      const newCustomer = result.rows[0];
 
-        res.status(201).json(newCustomer);
+      // Invalidate the cache
+      await redisClient.del("all_customers");
+      await redisClient.del(`customers_paginated_*`);
+
+      // Écrire le nouveau client dans le fichier CSV
+      const clientName = newCustomer.Name;
+      const cleanedClientName = clientName.replace(/[^a-zA-Z0-9]/g, "");
+      const filePath = path.join(
+        __dirname,
+        "success",
+        `Customers_${cleanedClientName}.csv`
+      );
+      writeCustomerToCSV(filePath, newCustomer);
+
+      res.status(201).json(newCustomer);
     } catch (error) {
-        console.error("Failed to add customer:", error);
-        const clientName = data.Name;
-        const cleanedClientName = clientName.replace(/[^a-zA-Z0-9]/g, '');
-        const filePath = path.join(
-            __dirname,
-            "error",
-            `Customers_${cleanedClientName}.csv`
-        );
-        writeCustomerToCSV(filePath, data);
-        res.status(500).send("Error adding customer");
+      console.error("Failed to add customer:", error);
+      const clientName = data.Name;
+      const cleanedClientName = clientName.replace(/[^a-zA-Z0-9]/g, "");
+      const filePath = path.join(
+        __dirname,
+        "error",
+        `Customers_${cleanedClientName}.csv`
+      );
+      writeCustomerToCSV(filePath, data);
+      res.status(500).send("Error adding customer");
     }
-},
+  },
 
   async getCustomersWithinRadius(req: Request, res: Response) {
     const { latCentral, lonCentral, rayonM } = req.query;
-  
+
     try {
       const lat = parseFloat(latCentral as string);
       const lon = parseFloat(lonCentral as string);
       const rayon = parseFloat(rayonM as string);
-  
+
       if (isNaN(lat) || isNaN(lon) || isNaN(rayon)) {
-        return res.status(400).json({ error: 'Invalid parameters' });
+        return res.status(400).json({ error: "Invalid parameters" });
       }
-  
+
       const query = `
         SELECT *
         FROM (
@@ -250,15 +260,22 @@ async updateCustomerById(req: Request, res: Response) {
       const result = await pgClient.query(query, values);
       return res.status(200).json(result.rows);
     } catch (err) {
-      console.error('Error fetching customers within radius:', err);
-      return res.status(500).json({ error: 'Internal Server Error' });
+      console.error("Error fetching customers within radius:", err);
+      return res.status(500).json({ error: "Internal Server Error" });
     }
-  }
+  },
 };
 
-function writeCustomerToCSV(filePath: string, customer: any): void {
-  const columns = Object.keys(customer).join(";");
+function writeCustomerToCSV(filePath, customer) {
+  const dir = path.dirname(filePath);
 
+  // Créer le répertoire s'il n'existe pas
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir, { recursive: true });
+  }
+
+  const columns = Object.keys(customer).join(";");
+  
   const values = Object.values(customer).map((value) => {
     let outputValue = "";
     if (typeof value === 'string') {
@@ -268,7 +285,6 @@ function writeCustomerToCSV(filePath: string, customer: any): void {
     } else {
       outputValue = JSON.stringify(value).replace(/"/g, '""');
     }
-
     return `"${outputValue}"`;
   }).join(";");
 
@@ -278,7 +294,13 @@ function writeCustomerToCSV(filePath: string, customer: any): void {
     const header = `${columns}\n`;
     fs.writeFileSync(filePath, header + dataLine, 'utf8');
   } else {
-    fs.appendFileSync(filePath, dataLine, 'utf8');
+    const existingData = fs.readFileSync(filePath, 'utf8');
+    const existingColumns = existingData.split('\n')[0];
+    if (existingColumns === columns) {
+      fs.appendFileSync(filePath, dataLine, 'utf8');
+    } else {
+      console.error("Column mismatch: The structure of the customer object does not match the existing file's columns.");
+    }
   }
 }
 
