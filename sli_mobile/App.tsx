0e1263d3811a8d7de8061aec2ad7df14245507f2
JordanSerafini <jordan@solution-logique.fr>
2024-07-25 17:22:39 +0200

    :construction: secure login

--- sli_mobile/App.tsx
+++ sli_mobile/App.tsx
@@ -1,20 +1,30 @@
 // App.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigationContainer from './NavigationContainer';
-import { ModalProvider } from './app/context/ModalContext';
-import ModalPortal from './app/Modal/ModalPortal';
+import { getCustomersPaginated } from './app/utils/functions/customer_functions';
 
 export default function App() {
+
+  useEffect(() => {
+    // Fonction asynchrone à l'intérieur de useEffect
+    const fetchData = async () => {
+      try {
+        await getCustomersPaginated("", 25, 0);
+      } catch (error) {
+        console.error('Error fetching customers:', error);
+      }
+    };
+
+    fetchData(); // Appel de la fonction asynchrone
+  }, []);
+
   return (
     <SafeAreaProvider>
-      <ModalProvider>
-        <View style={styles.container}>
-          <AppNavigationContainer />
-          <ModalPortal />
-        </View>
-      </ModalProvider>
+      <View style={styles.container}>
+        <AppNavigationContainer />
+      </View>
     </SafeAreaProvider>
   );
 }
