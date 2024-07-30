11209436d2846842fdaca29e709bc3f4d6dbf687
JordanSerafini <jordan@solution-logique.fr>
2024-07-30 17:14:36 +0200

    Ajout du backupNew.sql avec Git LFS et modifications récentes

--- backend/database/sql/fix.sql
+++ backend/database/sql/fix.sql
@@ -1,30 +1,3 @@
-BEGIN;
-
-
-    CREATE TABLE "Utilisateurs" (
-        id SERIAL PRIMARY KEY,
-        nom VARCHAR(255) NOT NULL,
-        prenom VARCHAR(255) NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        role VARCHAR(255) NOT NULL,
-        telephone VARCHAR(255),
-        NbrOfAttemps INTEGER,
-        isBLocked BOOLEAN,
-        BlockTime TIMESTAMP,
-        Token TEXT
-    );
-
-
-ALTER TABLE "FicheChantier" ADD COLUMN "imageUrl" TEXT;
-ALTER TABLE "FicheChantier" ADD COLUMN "audioUrl" TEXT;
-
-
-
-ALTER TABLE "Item" DROP COLUMN IF EXISTS "Id";
-ALTER TABLE "Item" ADD COLUMN "Id" SERIAL PRIMARY KEY;
-
-ALTER TABLE "Customer" ADD COLUMN "Lat" DOUBLE PRECISION;
-ALTER TABLE "Customer" ADD COLUMN "Lon" DOUBLE PRECISION;
-
-COMMIT;
+version https://git-lfs.github.com/spec/v1
+oid sha256:17876dfbf54510d2b0f7e3b2bfb0b5a79353669dcc18140e004fc80a6152ad17
+size 784
