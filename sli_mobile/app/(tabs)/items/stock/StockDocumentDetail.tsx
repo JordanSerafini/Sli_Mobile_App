9772a2f0cac8f7a717f7d91fa6147eeee2e76254
JordanSerafini <jordan@solution-logique.fr>
2024-07-31 17:28:48 +0200

    working on stock doc query :sparkles: seems ok

--- sli_mobile/app/(tabs)/items/stock/StockDocumentDetail.tsx
+++ sli_mobile/app/(tabs)/items/stock/StockDocumentDetail.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View } from 'react-native'
+import { getStockDocByID } from '../../../utils/functions/stock_function'
 
 function StockDocumentDetail() {
+
+  const [stockDoc, setStockDoc] = useState(null);
+
+  useEffect(() => {
+    const fetchStockDoc = async () => {
+      const data = await getStockDocByID('deb5ccaf-5574-4175-a0ee-ce453e3732e3');
+      setStockDoc(data);
+    };
+
+    fetchStockDoc();
+  }
+  , []);
+
   return (
     <View>
         
