4606065d90b1221bea294387ece9f77cc05fbcc1
JordanSerafini <jordan@solution-logique.fr>
2024-08-02 15:27:59 +0200

    add useSegment

--- sli_mobile/app/(tabs)/items/index.tsx
+++ sli_mobile/app/(tabs)/items/index.tsx
@@ -1,42 +1,57 @@
-import { router } from 'expo-router';
-import React from 'react';
+import { router, useRouter, useSegments } from 'expo-router';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import FabItem from './FAB/FabItem';
 import ButtonPerso from '../../components/UI/ButtonPerso';
-import Icon from "react-native-vector-icons/AntDesign";
+import StockDocumentList from './StockDocumentList';
+import ItemList from './itemList';
 
 const IndexScreen: React.FC = () => {
+  const [content, setContent] = useState<React.ReactNode>(null);
+  const segments = useSegments(); 
 
   const itemClick = () => {
-    router.push('/items/itemList');
+    setContent(<ItemList />);
   };
 
   const stockClick = () => {
-    router.push('/items/StockDocumentList');
+    setContent(<StockDocumentList />);
   };
+
+  useEffect(() => {
+    // Reset content when the segments change (i.e., navigation changes)
+    setContent(null);
+  }, [segments]);
 
   const theme = {
     ...DefaultTheme,
     colors: {
       ...DefaultTheme.colors,
-      primary: '#1e40af', 
+      primary: '#1e40af',
       secondary: '#BFDBFE',
-      accent: '#1E3A8A', 
+      accent: '#1E3A8A',
     },
   };
 
   return (
     <PaperProvider theme={theme}>
-      <SafeAreaView style={styles.container}>
-        <ButtonPerso mode="outlined" icon="account" text="Liste des articles" css="w-4.5/10 self-center" onPress={itemClick} />
-        <ButtonPerso mode="outlined" icon="account" text="STOCK" css="w-4.5/10 self-center" onPress={stockClick} />
-        <FabItem 
-          showAddModal={() => {}} 
-          showEditItemModal={() => {}} 
-          showAddItemModal={() => {}} 
-        />
-      </SafeAreaView>
+      {!content ? (
+        <SafeAreaView style={styles.container}>
+          <ButtonPerso mode="outlined" icon="account" text="Liste des articles" css="w-4.5/10 self-center" onPress={itemClick} />
+          <ButtonPerso mode="outlined" icon="account" text="STOCK" css="w-4.5/10 self-center" onPress={stockClick} />
+          
+        </SafeAreaView>
+      ) : (
+        <SafeAreaView style={styles.container}>
+          {content}
+        </SafeAreaView>
+      )}
+      <FabItem
+            showAddModal={() => {}}
+            showEditItemModal={() => {}}
+            showAddItemModal={() => {}}
+          />
     </PaperProvider>
   );
 };
--- sli_mobile/app/(tabs)/items/itemList.tsx
+++ sli_mobile/app/(tabs)/items/itemList.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { View } from "react-native"
 
 function itemList() {
   return (
-    <div>itemList</div>
+    <View>
+      
+    </View>
   )
 }
 
