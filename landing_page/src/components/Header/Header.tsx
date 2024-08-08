269cc25f8765913a7d9fd6783b4168ff779058cc
JordanSerafini <jordan@solution-logique.fr>
2024-08-08 09:17:09 +0200

    landing page carrosuel

--- landing_page/src/components/Header/Header.tsx
+++ landing_page/src/components/Header/Header.tsx
@@ -5,14 +5,14 @@ import phone from "../../assets/phoneIcon.png";
 
 function Header() {
   return (
-    <div className="w-full h-1/10 flex justify-evenly items-center">
-      <div className="w-1/10">
+    <div className="w-screen p-4 h-1/10 flex justify-between items-center">
+      <div className="w-1/10 items-center flex justify-center">
         <img src={sli} alt="sli_image" className="h-14 rounded-full" />
       </div>
-      <div className="w-6/10">
+      <div className="w-5/10">
         <Navbar />
       </div>
-      <div className="flex gap-4">
+      <div className="w-1/10 items-center flex justify-center gap-4" >
         < img src={phone} alt="phone_icon" className="h-10 hover:scale-110 cursor-pointer" />
         <div className="rounded-xl text-white bg-blue-800 flex items-center justify-center px-4 py-2 cursor-pointer hover:scale-105 ">Essaie</div>
       </div>
