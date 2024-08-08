57b877222ba1f23e93390f6156d0c2fc34c37613
JordanSerafini <jordan@solution-logique.fr>
2024-08-08 09:19:48 +0200

    landing page carrosuel

--- landing_page/src/components/Home/Presentation.tsx
+++ landing_page/src/components/Home/Presentation.tsx
@@ -11,17 +11,22 @@ import {
 const items = [
   {
     title: "La puissance de votre logiciel, en toute mobilité.",
-    text: "Transformez votre façon de travailler avec notre application mobile. Profitez de toutes les fonctionnalités de votre logiciel de bureau, où que vous soyez, et boostez votre productivité en toute simplicité.",
+    text: "Transformez votre manière de travailler avec notre application mobile. Profitez de toutes les fonctionnalités de votre logiciel de bureau, où que vous soyez, et améliorez votre productivité sans effort.",
     images: [mobilePic1, mobilePic2],
   },
   {
     title: "Gestion commerciale et de stock",
-    text: "Gérez efficacement vos clients, vos articles, et vos stocks avec notre solution mobile. Accédez à toutes les fonctionnalités de gestion commerciale où que vous soyez et assurez un suivi précis et à jour de vos opérations.",
+    text: "Optimisez la gestion de vos clients, articles et stocks avec notre solution mobile. Accédez à toutes les fonctionnalités de gestion commerciale où que vous soyez et assurez un suivi précis et actualisé de vos opérations.",
     images: [mobilePic1, mobilePic2],
   },
   {
     title: "Gestion de chantier et devis",
-    text: "Simplifiez la gestion de vos chantiers et l'établissement de vos devis grâce à notre application mobile. Profitez d'outils dédiés pour organiser vos projets, suivre l'avancement des travaux, et générer des devis précis et détaillés.",
+    text: "Simplifiez la gestion de vos chantiers et l'établissement de devis grâce à notre application mobile. Utilisez des outils dédiés pour organiser vos projets, suivre l'avancement des travaux et générer des devis détaillés.",
+    images: [mobilePic1, mobilePic2],
+  },
+  {
+    title: "Gestion de planning et rendez-vous",
+    text: "Gérez efficacement vos plannings et rendez-vous avec notre solution mobile. Planifiez, suivez et ajustez vos rendez-vous en temps réel pour une organisation sans faille.",
     images: [mobilePic1, mobilePic2],
   },
 ];
@@ -31,10 +36,10 @@ function Carrousel() {
     <Carousel className="w-full max-w-9/10" opts={{ loop: true }}>
       <CarouselContent className="">
         {items.map((item, index) => (
-          <CarouselItem key={index} className="flex items-center justify-center">
+          <CarouselItem key={index} className="flex items-center justify-center mb-28">
             <div className="flex-col flex gap-8 w-5/10 items-center">
               <h2 className="text-6xl text-center font-bold">{item.title}</h2>
-              <p className="text-xl text-center w-7/10">{item.text}</p>
+              <p className="text-xl text-center w-9/10">{item.text}</p>
               <div className="flex gap-8 w-full justify-evenly mt-8">
                 <img src={item.images[0]} alt="" className="h-96" />
                 <img src={item.images[1]} alt="" className="h-96" />
